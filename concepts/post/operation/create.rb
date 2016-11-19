c9f911c80871717acf27940bcb850b28e501c8cf
Phillip Oertel <me@phillipoertel.com>
2016-11-19 11:48:24 +0100

    Update validations to latest dry validation API

--- concepts/post/operation/create.rb
+++ concepts/post/operation/create.rb
@@ -9,16 +9,16 @@ class Post::Create < Trailblazer::Operation
     include Reform::Form::Dry::Validations
 
     validation :default do
-      key(:title, &:filled?)
-      key(:url_slug) { |slug| slug.format?(/^[\w-]+$/) && slug.unique? }
-
-      def unique?(value)
-        form.model.class[url_slug: value].nil?
+      configure do
+        def unique?(value)
+          form.model.class[url_slug: value].nil?
+        end
+        config.messages_file = 'concepts/post/operation/dry_error_messages.yml'
       end
 
-      configure { |config|
-        config.messages_file = 'concepts/post/operation/dry_error_messages.yml'
-      }
+      required(:title).value(:filled?)
+      required(:url_slug).value(format?: /^[\w-]+$/)
+      required(:url_slug).filled(:unique?)
     end
   end
 
